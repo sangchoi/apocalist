7f56061f32e088fb372afdd978128c90301b3590
orjames <orjames@berkeley.edu>
2019-03-28 16:19:51 -0700

    starting design of components

---
+++ src/Categories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Procedures = (props) => {
+  // const procedures = props.procedures.map((procedure, index) => {
+  //     return <li key={index}><Link to={`/procedures/${procedure.id}`}>{procedure.name}</Link></li>
+  // })
+  return (
+    <h1>categories</h1>
+    // <div className="App">
+    //  <h1>Things we do that hurt:</h1>
+    //  <ul>
+    //      {procedures}
+    //  </ul>
+    // </div>
+  );
+};
+
+export default Procedures;
